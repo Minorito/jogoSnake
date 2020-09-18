982173d8c6d1b22d50a2b46f05290f933f5aaa6a
SamuelPiasecki <samupiasecki@gmail.com>
2020-09-18 17:10:54 -0300

    menu

---
+++ menu.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <conio.h>
+#define MAX 2
+#define MIN 1
+
+void arrowHere(int realPosition, int arrowPosition)
+{
+    if (realPosition == arrowPosition)
+    {
+        printf(">>");
+    }
+    else
+    {
+        printf("  ");
+    }
+}
+
+void menu()
+{
+    system("cls");
+    int position = 1;
+    int keyPressed = 0;
+
+    while (keyPressed != 13)
+    {
+        system("cls");
+        arrowHere(1, position);
+        printf("INICIAR\n");
+        arrowHere(2, position);
+        printf("SAIR DO JOGO\n");
+
+        keyPressed = getch();
+        fflush(stdin);
+
+        if (keyPressed == 80 && position != MAX)
+        {
+            position++;
+        }
+        else if (keyPressed == 72 && position != MIN)
+        {
+            position--;
+        }
+        else
+        {
+            position = position;
+        }
+    }
+}
+
+int main()
+{
+
+    menu();
+    return 0;
+}
